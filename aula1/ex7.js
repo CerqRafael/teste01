542ea69cd52294f028e20a79fa2a6497c2444e16
Rafael <rafael.caraujo9@senacsp.edu.br>
2023-11-07 20:23:55 -0300

    Alteracao var hip - adicionando os arquivos do repositorio

--- aula1/ex7.js
+++ aula1/ex7.js
@@ -1,4 +1,4 @@
 const prompt = require("prompt-sync")();
 var catOP = parseFloat(prompt("Digite o valor do cateto oposto: "));
 var catAD = parseFloat(prompt("Digite o valor do cateto adjacente: "));
-var hip = parseFloat(catOP**2 + catAD**2) ** (1/2);
+var hipotenusa = parseFloat(catOP**2 + catAD**2) ** (1/2);
